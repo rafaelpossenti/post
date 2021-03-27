b8fe7ae32eb919658e04ab94248b6cb5b38d1c7c
rafael.possenti <rafael_possenti@hotmail.com>
2021-03-27 09:30:33 -0300

    first commit

---
+++ src/test/kotlin/com/possenti/post/PostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.possenti.post
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
