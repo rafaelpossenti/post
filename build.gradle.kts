38cf503a593da4b798f41fcc48a346c609d3e554
rafael.possenti <rafael41051010>
2021-07-21 18:43:21 -0300

    actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ extra["retrofitVersion"] = "2.5.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 
 	implementation("org.springframework.cloud:spring-cloud-starter")
